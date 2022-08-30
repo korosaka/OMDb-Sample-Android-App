e1c1e675604a8063375e0548b77a1fa9dfb22ded
koro <koro.nn99@gmail.com>
2022-08-30 13:25:38 -0700

    Implement DataBinding on Recycler ItemView

--- app/src/main/java/com/lyft/android/omdbsampleproject/view/adapter/MovieListAdapter.kt
+++ app/src/main/java/com/lyft/android/omdbsampleproject/view/adapter/MovieListAdapter.kt
@@ -5,29 +5,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.lyft.android.omdbsampleproject.R
+import com.lyft.android.omdbsampleproject.databinding.MovieListItemViewBinding
 import com.lyft.android.omdbsampleproject.model.MovieData
 import kotlinx.android.synthetic.main.movie_list_item_view.view.*
 
 class MovieListAdapter(private val movies: List<MovieData>) :
     RecyclerView.Adapter<MovieListAdapter.MyViewHolder>() {
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class MyViewHolder(val binding: MovieListItemViewBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
         return MyViewHolder(
-            LayoutInflater.from(parent.context)
-                .inflate(R.layout.movie_list_item_view, parent, false)
+            MovieListItemViewBinding.inflate(layoutInflater, parent, false)
         )
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val movie = movies[position]
-
-        holder.itemView.movie_title.text = movie.title
-        holder.itemView.movie_year.text = movie.year
-        movie.poster?.let {
-            holder.itemView.movie_poster_image.setImageBitmap(it)
-        }
+        holder.binding.movieData = movie
     }
 
     override fun getItemCount(): Int {
