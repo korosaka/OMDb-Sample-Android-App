9ee867c1ad7e19f182ffd5702a33421ac16fd427
koro <koro.nn99@gmail.com>
2022-08-29 21:29:20 -0700

    Unit test for ViewModel

---
+++ app/src/test/java/com/lyft/android/omdbsampleproject/view_model/SearchingMoviesViewModelTest.kt
@@ -0,0 +1,27 @@
+package com.lyft.android.omdbsampleproject.view_model
+
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+internal class SearchingMoviesViewModelTest {
+
+    private lateinit var viewModel: SearchingMoviesViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = SearchingMoviesViewModel()
+    }
+
+    @Test
+    fun fetchMoviesTest() {
+        runBlocking {
+            viewModel.fetchMovies()
+        }
+        assertEquals(10, viewModel.movies.size)
+        assertTrue(viewModel.movies.size == 10)
+    }
+
+}
